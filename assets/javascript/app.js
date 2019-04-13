bbf88386977c106c14213efb24a1897146a87d39
Patience D <patience.deaton2@outlook.com>
2019-04-13 11:43:09 -0400

    container changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,20 +24,22 @@ var forecastConditionsUrl =
   myLocation +
   "A9IYPehiyBlSicaf0AMQF9lZsMQMnLnH";
 
-$("#location-search").on("click", function(event) {
+$("#location-search").on("click", function (event) {
   console.log("Clicked...");
 
   event.preventDefault();
-  var myLocation = $("#the-real-location")
-    .val()
-    .trim();
+  var myLocation = $("#the-real-location").val().trim();
+
   console.log(myLocation);
-  var eventbriteURL =
-    "https://www.eventbriteapi.com/v3/events/search/?location.address=" +
-    myLocation +
+
+  var eventbriteURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + myLocation +
     "&location.within=10mi&token=S5ODNTLSPUBRNVOLMPSS";
+
   ajaxCall(eventbriteURL);
   console.log(eventbriteURL);
+
+  
+
 });
 
 function ajaxCall(url) {
@@ -46,7 +48,7 @@ function ajaxCall(url) {
   $.ajax({
     url: url,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     console.log("test");
     console.log(response);
   });
